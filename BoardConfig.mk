9ffe3468088c4dab0702ef77a52e645744b84e0b
Arian <arian.kulmer@web.de>
2023-05-11 19:32:30 +0200

    unicorn: Configure screen density
    Set the screen density according to ro.sf.lcd_density from miui.
    
    Change-Id: Ic11b052d132f150da7070e5b8669e5a229f7528d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,3 +11,6 @@ include device/xiaomi/sm8450-common/BoardConfigCommon.mk
 include vendor/xiaomi/unicorn/BoardConfigVendor.mk
 
 DEVICE_PATH := device/xiaomi/unicorn
+
+# Screen density
+TARGET_SCREEN_DENSITY := 560
