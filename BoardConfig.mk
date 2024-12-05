deb028dabd354f4456cfdf544b1f5ca1e774d45d
Arian <arian.kulmer@web.de>
2024-12-05 18:55:59 +0100

    unicorn: Commonize kernel module load list
    Change-Id: I13e6b6c9ab106d91b10d5e25dd92d86698da6bb0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,21 +12,6 @@ include vendor/xiaomi/unicorn/BoardConfigVendor.mk
 
 DEVICE_PATH := device/xiaomi/unicorn
 
-# Kernel
-device_second_stage_modules := \
-    goodix_fod.ko \
-    qcom_pm8008-regulator.ko \
-    fts_touch_spi.ko \
-    aw8697-haptic.ko
-
-device_vendor_dlkm_exclusive_modules := \
-    cs35l41_dlkm.ko
-
-BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD += $(device_second_stage_modules)
-BOARD_VENDOR_KERNEL_MODULES_LOAD += $(device_second_stage_modules) $(device_vendor_dlkm_exclusive_modules)
-
-BOOT_KERNEL_MODULES += $(device_second_stage_modules)
-
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/properties/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/properties/vendor.prop
